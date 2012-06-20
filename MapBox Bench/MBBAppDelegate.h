88034f5e4589bcc2215313a070ac33a3b1fbfeca
Justin R. Miller <incanus@codesorcery.net>
2012-06-20 15:15:41 -0700

    basic app setup

--- MapBox Bench/MBBAppDelegate.h
+++ MapBox Bench/MBBAppDelegate.h
@@ -8,12 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@class MBBViewController;
-
 @interface MBBAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (strong, nonatomic) MBBViewController *viewController;
 
 @end
--- MapBox Bench/MBBViewController.m
+++ MapBox Bench/MBBViewController.m
@@ -8,31 +8,45 @@
 
 #import "MBBViewController.h"
 
+#import "RMMapView.h"
+#import "RMMapBoxSource.h"
+
+#define kNormalSourceURL [NSURL URLWithString:@"http://a.tiles.mapbox.com/v3/justin.map-s2effxa8.jsonp"] // see https://tiles.mapbox.com/justin/map/map-s2effxa8
+#define kRetinaSourceURL [NSURL URLWithString:@"http://a.tiles.mapbox.com/v3/justin.map-kswgei2n.jsonp"] // see https://tiles.mapbox.com/justin/map/map-kswgei2n
+
 @interface MBBViewController ()
+
+@property (strong, nonatomic) RMMapView *mapView;
 
 @end
 
+#pragma mark -
+
 @implementation MBBViewController
+
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    RMMapBoxSource *onlineSource = [[RMMapBoxSource alloc] initWithReferenceURL:(([[UIScreen mainScreen] scale] > 1.0) ? kRetinaSourceURL : kNormalSourceURL)];
+    
+    self.mapView = [[RMMapView alloc] initWithFrame:self.view.bounds andTilesource:onlineSource];
+    
+    self.mapView.zoom = 2;
+    
+    self.mapView.backgroundColor = [UIColor darkGrayColor];
+    
+    self.mapView.decelerationMode = RMMapDecelerationFast;
+    
+    self.mapView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    
+    self.mapView.boundingMask = RMMapMinHeightBound;
+    
+    self.mapView.viewControllerPresentingAttribution = self;
+    
+    [self.view addSubview:self.mapView];
 }
 
 @end

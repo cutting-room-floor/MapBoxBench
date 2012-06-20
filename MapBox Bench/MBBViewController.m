9dce8904333f25b3e3b6926e7a1ec1aed1f0b43a
Justin R. Miller <incanus@codesorcery.net>
2012-06-20 15:37:08 -0700

    basic cache empty & options buttons

--- MapBox Bench/MBBViewController.m
+++ MapBox Bench/MBBViewController.m
@@ -11,8 +11,35 @@
 #import "RMMapView.h"
 #import "RMMapBoxSource.h"
 
+#import <QuartzCore/QuartzCore.h>
+
 #define kNormalSourceURL [NSURL URLWithString:@"http://a.tiles.mapbox.com/v3/justin.map-s2effxa8.jsonp"] // see https://tiles.mapbox.com/justin/map/map-s2effxa8
 #define kRetinaSourceURL [NSURL URLWithString:@"http://a.tiles.mapbox.com/v3/justin.map-kswgei2n.jsonp"] // see https://tiles.mapbox.com/justin/map/map-kswgei2n
+
+#pragma mark -
+
+@interface RMMapView (BenchExtensions)
+
+- (void)emptyCacheAndForceRefresh;
+
+@end
+
+#pragma mark -
+
+@implementation RMMapView (BenchExtensions)
+
+- (void)emptyCacheAndForceRefresh
+{
+    [self removeAllCachedImages];
+    
+    tiledLayerView.layer.contents = nil;
+    
+    [tiledLayerView.layer setNeedsDisplay];
+}
+
+@end
+
+#pragma mark -
 
 @interface MBBViewController ()
 
@@ -47,6 +74,22 @@
     self.mapView.viewControllerPresentingAttribution = self;
     
     [self.view addSubview:self.mapView];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Empty Cache" style:UIBarButtonItemStyleBordered target:self action:@selector(emptyCache:)];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Options" style:UIBarButtonItemStyleBordered target:self action:@selector(showOptions:)];
+}
+
+#pragma mark -
+
+- (void)emptyCache:(id)sender
+{
+    [self.mapView emptyCacheAndForceRefresh];
+}
+
+- (void)showOptions:(id)sender
+{
+    NSLog(@"blah");
 }
 
 @end
